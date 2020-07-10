1c91b94fe9a28cbe6f276999cc420640217b2347
Herb Miller <herb@bobbingwide.com>
2020-07-10 07:58:44 +0100

    Update build for development version

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'ad515c47ff8c3c165640ece6019c7dc5');
+<?php return array('dependencies' => array('wp-blocks', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3191fc356c28e8c96cd2c3b97dbbb071');
