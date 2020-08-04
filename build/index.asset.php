2a328ac38c2a7f4454d8f8cc7c2785df3734eb60
Herb Miller <herb@bobbingwide.com>
2020-08-04 12:49:29 +0100

    Update build files to production version. built with npm run build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '586f7cf9ec53965f479c64d61b7e30df');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '35436cf87ded48edda999df56161d449');
